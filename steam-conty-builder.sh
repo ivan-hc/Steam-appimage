3ebece43e5ca7522e220b792bf2b3a3d2c973035
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-06 00:32:11 +0200

    Update steam-conty-builder.sh

--- steam-conty-builder.sh
+++ steam-conty-builder.sh
@@ -335,6 +335,6 @@ cd .. || exit 1
 
 # EXPORT THE APPDIR TO AN APPIMAGE
 VERSION=$(curl -Ls https://archlinux.org/packages/multilib/x86_64/steam/ | grep steam | head -1 | tr ' ' '\n' | grep "^[0-9]")
-ARCH=x86_64 VERSION="$VERSION-1" ./appimagetool -s ./"$APP".AppDir
+ARCH=x86_64 VERSION="$VERSION-2" ./appimagetool -s ./"$APP".AppDir
 cd .. && mv ./tmp/*.AppImage ./ || exit 1
 
