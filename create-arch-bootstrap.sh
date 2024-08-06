10a02a7d3edf9fd9c01de0ac6aece1097af2ac76
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-06 04:28:34 +0200

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -33,8 +33,7 @@ devel_pkgs="base-devel"
 # packages from the Chaotic-AUR repo
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
 	which ttf-dejavu ttf-liberation steam xorg-xwayland wayland \
-	lib32-wayland gamescope gamemode lib32-gamemode mangohud lib32-mangohud \
-	lib32-glibc"
+	lib32-wayland gamescope gamemode lib32-gamemode mangohud lib32-mangohud"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist="glibc-eac-bin lib32-glibc-eac-bin zenity-gtk3 steam-screensaver-fix"
