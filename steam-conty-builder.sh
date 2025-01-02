cecd8f81d2d3ed0f636c9a5f29f9ec36b9ea74de
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-02 04:03:12 +0100

    Update steam-conty-builder.sh
    - use Archimage 4.1 Nvidia handler
    - disable NVIDIA_HANDLER

--- steam-conty-builder.sh
+++ steam-conty-builder.sh
@@ -316,7 +316,33 @@ cat >> ./AppRun << 'EOF'
 #!/bin/sh
 HERE="$(dirname "$(readlink -f "${0}")")"
 export UNION_PRELOAD="${HERE}"
-"${HERE}"/conty.sh -- steam-screensaver-fix-runtime "$@"
+
+[ -z "$NVIDIA_ON" ] && NVIDIA_ON=1
+if [ "$NVIDIA_ON" = 1 ]; then
+   DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+   CONTY_DIR="${DATADIR}/Conty/overlayfs_shared"
+   [ -f /sys/module/nvidia/version ] && nvidia_driver_version="$(cat /sys/module/nvidia/version)"
+   if [ -n "$nvidia_driver_version" ]; then
+      mkdir -p "${CONTY_DIR}"/nvidia "${CONTY_DIR}"/up/usr/lib "${CONTY_DIR}"/up/usr/share
+      nvidia_data_dirs="egl glvnd nvidia vulkan"
+      for d in $nvidia_data_dirs; do [ ! -d "${CONTY_DIR}"/up/usr/share/"$d" ] && ln -s /usr/share/"$d" "${CONTY_DIR}"/up/usr/share/ 2>/dev/null; done
+      [ ! -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
+      [ -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && nvidia_driver_conty=$(cat "${CONTY_DIR}"/nvidia/current-nvidia-version)
+      if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
+         rm -f "${CONTY_DIR}"/up/usr/lib/*; echo "${nvidia_driver_version}" > "${CONTY_DIR}"/nvidia/current-nvidia-version
+      fi
+      /sbin/ldconfig -p > "${CONTY_DIR}"/nvidia/host_libs
+      grep -i "nvidia\|libcuda" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2 > "${CONTY_DIR}"/nvidia/host_nvidia_libs
+      libnv_paths=$(grep "libnv" "${CONTY_DIR}"/nvidia/host_libs | cut -d ">" -f 2)
+      for f in $libnv_paths; do strings "${f}" | grep -qi -m 1 "nvidia" && echo "${f}" >> "${CONTY_DIR}"/nvidia/host_nvidia_libs; done
+      nvidia_libs=$(cat "${CONTY_DIR}"/nvidia/host_nvidia_libs)
+      for n in $nvidia_libs; do libname=$(echo "$n" | sed 's:.*/::') && [ ! -f "${CONTY_DIR}"/up/usr/lib/"$libname" ] && cp "$n" "${CONTY_DIR}"/up/usr/lib/; done
+      [ -d "${CONTY_DIR}"/up/usr/lib ] && export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${CONTY_DIR}"/up/usr/lib:"${LD_LIBRARY_PATH}"
+      [ -d "${CONTY_DIR}"/up/usr/share ] && export XDG_DATA_DIRS="${XDG_DATA_DIRS}":"${CONTY_DIR}"/up/usr/share:"${XDG_DATA_DIRS}"
+   fi
+fi
+
+NVIDIA_HANDLER=0 "${HERE}"/conty.sh -- steam-screensaver-fix-runtime "$@"
 EOF
 chmod a+x ./AppRun
 
