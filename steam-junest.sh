75f41804cb089d0e7ab8fd00abec8a41c1869a5a
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-02 00:40:45 +0100

    Update steam-junest.sh

--- steam-junest.sh
+++ steam-junest.sh
@@ -5,8 +5,9 @@ BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for 
 lib32_pkgs="lib32-alsa-lib lib32-giflib lib32-gnutls lib32-libjpeg-turbo lib32-libldap lib32-libpng lib32-libpulse lib32-libva \
 lib32-libva-intel-driver lib32-libva-mesa-driver lib32-libxcomposite lib32-libxslt lib32-mesa lib32-mpg123 lib32-pipewire \
 lib32-v4l-utils lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-intel lib32-vulkan-mesa-layers lib32-vulkan-radeon \
-lib32-gamemode mangohud lib32-mangohud lib32-wayland lib32-gtk2 lib32-libtiff"
-DEPENDENCES="gamescope gamemode glibc-eac-bin lib32-glibc-eac-bin libyaml pciutils steam tar xz yad git xdotool $lib32_pkgs" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+lib32-gamemode lib32-mangohud lib32-wayland lib32-gtk2 lib32-libtiff lib32-glibc-eac-bin"
+video_pkgs="mesa vulkan-radeon vulkan-intel vulkan-icd-loader vulkan-mesa-layers libva-mesa-driver libva-intel-driver intel-media-driver"
+DEPENDENCES="gamescope gamemode git glibc-eac-bin libyaml mangohud pciutils steam tar wayland xz xdotool xorg-xwayland yad $lib32_pkgs $video_pkgs" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
 COMPILERS="base-devel"
 
@@ -14,7 +15,7 @@ COMPILERS="base-devel"
 #	KEYWORDS TO FIND AND SAVE WHEN COMPILING THE APPIMAGE
 #############################################################################
 
-BINSAVED="mktemp md5sum cmp getopt zenity lspci"
+BINSAVED="mktemp md5sum cmp getopt zenity lspci steam"
 SHARESAVED="SAVESHAREPLEASE"
 lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
