dc8c5a09f638859c77eeea113ad05c946ca1e4a2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-11 06:51:27 -0400

    high `-B` isn't needed anymore

--- steam-runimage.sh
+++ steam-runimage.sh
@@ -139,7 +139,7 @@ echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B32 \
+	--compression zstd:level=22 -S26 -B8 \
 	--header uruntime \
 	-i ./AppDir -o Steam-"$VERSION"-anylinux.dwarfs-"$ARCH".AppImage
 
