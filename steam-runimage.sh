b44cb8c2cf5e7d98e404c8e428451d78c87160e3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-06 15:12:10 -0400

    fix indentation in heredoc declaration

--- steam-runimage.sh
+++ steam-runimage.sh
@@ -72,7 +72,7 @@ run_install() {
 	RIM_SHARE_FONTS="${RIM_SHARE_FONTS:=1}"
 	RIM_SHARE_THEMES="${RIM_SHARE_THEMES:=1}"
 	RIM_ALLOW_ROOT="${RIM_ALLOW_ROOT:=1}"
-        RIM_BIND="/usr/share/locale:/usr/share/locale,/usr/lib/locale:/usr/lib/locale"
+	RIM_BIND="/usr/share/locale:/usr/share/locale,/usr/lib/locale:/usr/lib/locale"
 	RIM_AUTORUN=steam-screensaver-fix-runtime
 	EOF
 
